d3eb8f70324437603d0649ded520f161f36d4a45
Caio Erthal <erthalcaio1@gmail.com>
2025-01-20 11:05:27 -0300

    Adicionando clubes

--- app/models/jogo.rb
+++ app/models/jogo.rb
@@ -1,3 +1,5 @@
 class Jogo < ApplicationRecord
     validates_with JogoValidator
+    belongs_to :clube_visitante, class_name: "Clube", foreign_key: "clube_visitante_id"
+    belongs_to :clube_mandante, class_name: "Clube", foreign_key: "clube_mandante_id"
 end
