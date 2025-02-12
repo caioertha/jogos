0bfd31d76be705fb2a92dd797a4a1e8625112630
Caio Erthal <erthalcaio1@gmail.com>
2025-02-12 11:47:47 -0300

    Organizando

--- app/models/clube.rb
+++ app/models/clube.rb
@@ -1,3 +1,4 @@
 class Clube < ApplicationRecord
-    has_many :jogos
+    has_many :jogos_mandante, class_name: "Jogo", foreign_key: "clube_mandante_id", dependent: :destroy
+    has_many :jogos_visitante, class_name: "Jogo", foreign_key: "clube_visitante_id", dependent: :destroy
 end
